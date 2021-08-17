78c18c40c5bd06d350f30bd421b56b48e93d4a02
yalcinPXL <yalcinPXL@gmail.com>
2021-08-18 00:14:35 +0200

    Add project files.

---
+++ JWTAuthBlazor/Controllers/LoginController.cs
@@ -0,0 +1,93 @@
+﻿using JWTAuthBlazor.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthBlazor.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login(string userId, string password)
+        {
+            UserModel model = new UserModel();
+            model.UserName = userId;
+            model.Password = password;
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(model);
+            if (user is null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+            return response;
+        }
+
+        [Authorize(Roles ="Admin")]
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            try
+            {
+                return Ok(value);
+            }
+            catch (Exception)
+            {
+
+                return NotFound();
+            }
+        }
+
+        private string GenerateJSONWebToken(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(
+                 issuer:_config["Jwt:Issuer"],
+                 audience: _config["Jwt:Issuer"],
+                 claims,
+                 expires:DateTime.Now.AddMinutes(20),
+                 signingCredentials:credentials
+                );
+            var encodedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodedToken;
+        }
+
+        private UserModel AuthenticateUser(UserModel model)
+        {
+            var user = new UserModel();
+            if (model.UserName=="yalcin" && model.Password=="123")
+            {
+                //Hard coded user
+                 user = new UserModel { UserName = "AshProgHelp", Email= "ashproghelp.com", Password="123",  Role="Admin" };
+            }
+            return user;
+        }
+    }
+}
---
+++ JWTAuthBlazor/Startup.cs
@@ -0,0 +1,78 @@
+using JWTAuthBlazor.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthBlazor
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(option=> {
+                    option.TokenValidationParameters = new TokenValidationParameters {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });//Add
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();//Add
+            app.UseAuthorization();//Add
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapDefaultControllerRoute();//Add
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
